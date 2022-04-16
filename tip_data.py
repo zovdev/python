c2c4db7c422f1d8266714ba334bfd9716e543f1f
ZD <49118258+zovdev@users.noreply.github.com>
2022-04-16 22:47:23 +0300

    Create tip_data.py

---
+++ tip_data.py
@@ -0,0 +1,25 @@
+# int(integer)
+number = 5
+age = 14
+
+# float
+fnumber = 3.5
+
+# str(string)
+name = "tema"
+
+# bool
+status = True
+
+# вывод комментарий
+print("-Как дела? :)")
+
+# Экранирование
+print("всё очень\"хорошо\"")  # "a\"b\"v" или "a'b'v"
+
+# Перевод строки
+print('-это\n-что делаешь?')
+
+# Конкатенация
+print("Привет,моё имя " + name)
+print("Мне " + str(age) + " лет")
